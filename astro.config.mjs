abc1e6b126c29f91ac3cf8874c816854a960a0fb
Lolikana <thomas.guiart@gmail.com>
2023-11-14 13:37:52 +0900

    setup astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,26 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [],
+	build: {
+		inlineStylesheets: 'auto'
+	},
+	// scopedStyleStrategy: 'where',
+	vite: {
+		resolve: {
+			alias: {
+				'@components': '/src/components',
+				'@layouts': '/src/layouts',
+				'@styles': '/src/styles',
+				'@scripts': '/src/scripts',
+				'@assets': '/src/assets'
+			}
+		}
+	},
+	image: {
+		domains: ['astro.build'],
+		remotePatterns: [{ protocol: 'https' }]
+	},
+	server: ({ command }) => ({ port: command === 'dev' ? 4321 : 4000, host: true })
+});
