983b9afb1f96b5b64b340bb640d079ece9e5edc3
Lolikana <thomas.guiart@gmail.com>
2023-11-24 15:16:00 +0900

    add sns bar

--- src/libs/data.ts
+++ src/libs/data.ts
@@ -1,4 +1,9 @@
-import type { TNavbarLinksData } from './types';
+import IconCodePen from '@assets/icon/codepen-icon.svg';
+import IconGithub from '@assets/icon/github-icon.svg';
+import IconInstagram from '@assets/icon/instagram-icon.svg';
+import IconLinkedIn from '@assets/icon/linkedin-icon.svg';
+
+import type { TNavbarLinksData, TSnsData } from './types';
 
 export const navbarLinksData: TNavbarLinksData[] = [
 	{
@@ -12,5 +17,28 @@ export const navbarLinksData: TNavbarLinksData[] = [
 	},
 	{
 		href: 'contact'
+	}
+];
+
+export const snsData: TSnsData[] = [
+	{
+		site: 'github',
+		link: 'https://github.com/lolikana',
+		icon: IconGithub
+	},
+	{
+		site: 'instagram',
+		link: 'https://www.instagram.com/lolikana/',
+		icon: IconInstagram
+	},
+	{
+		site: 'codepen',
+		link: 'https://codepen.io/lolikana',
+		icon: IconCodePen
+	},
+	{
+		site: 'linkedin',
+		link: 'https://www.linkedin.com/in/thomas-guiart/',
+		icon: IconLinkedIn
 	}
 ];
--- src/libs/types.ts
+++ src/libs/types.ts
@@ -1,3 +1,11 @@
+import type { ImageMetadata } from 'astro';
+
 export type TNavbarLinksData = {
 	href: 'about' | 'experiences' | 'works' | 'contact';
+};
+
+export type TSnsData = {
+	site: 'github' | 'linkedin' | 'codepen' | 'instagram';
+	link: string;
+	icon: ImageMetadata;
 };
