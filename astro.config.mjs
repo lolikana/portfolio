a799e27ea3d17ac6ddd5efa6a28f84c51f068462
Lolikana <thomas.guiart@gmail.com>
2023-12-06 14:28:10 +0900

    export to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,7 +32,7 @@ export default defineConfig({
 		domains: ['astro.build'],
 		remotePatterns: [{ protocol: 'https' }]
 	},
-	server: ({ command }) => ({ port: command === 'dev' ? 4321 : 4000, host: true }),
-	site: import.meta.env.PROD ? 'https://lolikana.github.io' : undefined,
-	base: import.meta.env.PROD ? '/portfolio' : undefined
+	server: ({ command }) => ({ port: command === 'dev' ? 4321 : 4000, host: true })
+	// site: import.meta.env.PROD ? 'https://lolikana.github.io' : undefined,
+	// base: import.meta.env.PROD ? '/portfolio' : undefined
 });
